fa1e4c13f1e10d72755ce9b3074b2d3c120b17e0
Joseph Sutera <joesutera11@gmail.com>
2022-07-13 22:39:53 -0400

    commit tree 130581f9b48630ff2ce02eee0daf622353d04ac4

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+function writeCards(arrayOfNames,event){
+    const greeting=[];
+    for(let x=0;x<arrayOfNames.length;x++){
+       greeting.push( `Thank you, ${arrayOfNames[x]}, for the wonderful ${event} gift!`);
+ }
+    return greeting 
+    
+}
+
+function countDown(number){
+while (number>=0){
+
+    console.log(number);
+number--;
+}
+}
+   
+
+
+
+
+
+
+
+  
